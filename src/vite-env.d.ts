556b2d89c5f165d9837e004a47968a8721b19b93
sungwoo <sw.jang@gongsaero.com>
2026-03-10 19:10:13 +0900

    chore: 환경 변수 파일 및 타입 정의 추가
    - .env.example, .env.production 파일 추가
    - vite-env.d.ts에 ImportMetaEnv 타입 정의

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,3 +4,21 @@ declare module '*.module.scss' {
   const classes: { [key: string]: string }
   export default classes
 }
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string
+  readonly VITE_API_VERSION?: string
+  readonly VITE_APP_NAME?: string
+  readonly VITE_APP_DESCRIPTION?: string
+  readonly VITE_ENV?: 'development' | 'production'
+  readonly VITE_SOURCEMAP?: string
+  readonly VITE_DEBUG?: string
+  readonly VITE_SHOW_DEV_TOOLS?: string
+  readonly VITE_TIMEZONE?: string
+  readonly VITE_GA_ID?: string
+  readonly VITE_SENTRY_DSN?: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
